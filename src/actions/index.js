dcee76716f16935ae37a787897773e8310b8d6a9
tugce <tugceozdemiir@hotmail.com>
2022-12-28 00:10:13 +0300

    tugce

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DİSPLAY="CLEAR_DİSPLAY";
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +10,11 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operation) => {
+    return({type:CHANGE_OPERATION, payload:operation});
+}
+export const clearDisplay= () => {
+    return({type:CLEAR_DİSPLAY});
 }
